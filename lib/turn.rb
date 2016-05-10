526d4178eadc5c932bf9d46df0c4d5cb22344aa3
hodgesa <ahodges230@gmail.com>
2016-05-10 12:27:56 +0000

    nitrous-backup at Tue May 10 12:27:56 UTC 2016

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+def display_board(board)
+  puts    " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts    "-----------"
+  puts    " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts    "-----------"
+  puts    " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+  if
+   valid_move?(board, position)
+    move(board, position, "X")
+     display_board(board)
+  else
+  turn(board)
+  end
+end
+
+def valid_move?(board, position)
+  position = position.to_i-1
+  position.between?(0,8) && !position_taken?(board, position)
+end
+
+def position_taken?(board, position)
+    if board[position] == " " || board[position] == ""  || board[position] == nil
+      return false
+    else
+      return true
+        if board[position] == "X" || board[position] == "O"
+          return true
+        else
+          return false
+        end
+    end
+end
+
+def move(board, position, token = "X")
+   position = position-1
+   board[position] = token
+end
+
